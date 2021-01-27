8287ff4d82a82e8bfd3997b49f80c0b5e1b0934a
Manuel Topete <manueltopt96@gmail.com>
2021-01-26 18:53:23 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# The Game of Life
+**Author:** Manuel E. Topete Ortega
+
+--
+
+Ruby version: 2.7.2
---
+++ app.rb
@@ -0,0 +1,9 @@
+class App
+  class << self
+    def start!(rows, cols)
+      puts 'Welcome to the Game of Life'
+    end
+  end
+end
+
+App.start!
