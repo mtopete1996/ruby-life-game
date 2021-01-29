4f0cfb9933a5b3beac59812fc3ae0dc342b2296d
Manuel Topete <manueltopt96@gmail.com>
2021-01-28 19:13:26 -0600

    Add check_neighbours method and its test

--- grid.rb
+++ grid.rb
@@ -1,31 +1,32 @@
+require './logger'
+
 class Grid
-  def initialize(rows, cols)
+  def initialize(rows, cols, obj = nil)
     @cols = cols
     @rows = rows
+    @object = object(obj)
   end
 
-  attr_reader :cols, :rows
-
-  def neighbours(row, col)
-    previous_row = row - 1
-    next_row = row + 1
-    previous_col = col - 1
-    next_col = col + 1
-
-    [
-      [previous_row, previous_col], [previous_row, col], [previous_row, next_col],
-      [row, previous_col], [row, next_col],
-      [next_row, previous_col], [next_row, col], [next_row, next_col]
-    ]
+  def check_neighbours(row, col)
+    alive = 0
+    neighbours(row, col).each do |cell|
+      next unless cell[0] && cell[1]
+      alive += 1 if object[cell[0]][cell[1]] == 1
+    end
+    alive
   end
 
-  def object
-    @object ||= (1..rows).map do
-      Array.new cols
+  def object(obj = nil)
+    @object ||= begin
+      return obj if obj
+
+      (1..rows).map do
+        Array.new cols
+      end
     end
   end
 
-  def randomize
+  def randomize!
     object.each do |row|
       row.each_index do |c_index|
         row[c_index] = rand(2)
@@ -35,7 +36,49 @@ class Grid
 
   class << self
     def setup!(rows, cols)
-      new(rows, cols).randomize
+      new(rows, cols).randomize!
     end
+  end
+
+  private
+
+  attr_reader :cols, :rows
+
+  def neighbours(row, col)
+    raise ArgumentError, 'Arguments row and/or col are offset' if row > rows || col > cols
+    neighbours_h = neighbours_hash(row, col)
+
+    [
+      [neighbours_h[:previous_row], neighbours_h[:previous_col]], [neighbours_h[:previous_row], col],
+      [neighbours_h[:previous_row], neighbours_h[:next_col]],
+      [row, neighbours_h[:previous_col]], [row, neighbours_h[:next_col]],
+      [neighbours_h[:next_row], neighbours_h[:previous_col]], [neighbours_h[:next_row], col],
+      [neighbours_h[:next_row], neighbours_h[:next_col]]
+    ]
+  end
+
+  def neighbours_hash(row, col)
+    { previous_row: previous_row(row), next_row: next_row(row), previous_col: previous_col(col),
+      next_col: next_col(col) }
+  end
+
+  def next_col(col)
+    next_r = col + 1
+    next_r if next_r < cols
+  end
+
+  def next_row(row)
+    next_r = row + 1
+    next_r if next_r < rows
+  end
+
+  def previous_col(col)
+    previous = col - 1
+    previous if previous >= 0
+  end
+
+  def previous_row(row)
+    previous = row - 1
+    previous if previous >= 0
   end
 end
--- tests/grid_test.rb
+++ tests/grid_test.rb
@@ -2,9 +2,14 @@ require 'minitest/autorun'
 require '../grid'
 
 class TestGrid < Minitest::Test
+  def test_check_neighbours
+    assert_equal [[0, 1, 1], [0, 0, 0], [0, 0, 0]], hardcode_grid.object
+    assert_equal 2, hardcode_grid.check_neighbours(1, 1)
+  end
+
   def test_grid_object_is_right_size
-    assert_equal 8, grid.rows
-    assert_equal 9, grid.cols
+    assert_equal 8, grid.send(:rows)
+    assert_equal 9, grid.send(:cols)
 
     assert_equal 8, grid.object.length
     assert_equal 9, grid.object[0].length
@@ -13,11 +18,11 @@ class TestGrid < Minitest::Test
   def test_neighbours_method
     neighbours = [[2, 2], [2, 3], [2, 4], [3, 2], [3, 4], [4, 2], [4, 3], [4, 4]]
 
-    assert_equal neighbours, grid.neighbours(3, 3)
+    assert_equal neighbours, grid.send(:neighbours, 3, 3)
   end
 
   def test_randomize_method
-    grid_setup = Grid.setup!(2, 3)
+    grid_setup = Grid.setup!(3, 3)
 
     grid_setup.each do |row|
       assert row.none? { |col| col > 1 }
@@ -32,6 +37,11 @@ class TestGrid < Minitest::Test
   end
 
   private
+
+  def hardcode_grid
+    obj = [[0, 1, 1], [0, 0, 0], [0, 0, 0]]
+    Grid.new(3, 3, obj)
+  end
 
   def grid
     @grid ||= Grid.new(8, 9)
