1b8e8dacfbb9c012e655e87d5696eadeef2719a4
Manuel Topete <manueltopt96@gmail.com>
2021-01-31 00:14:22 -0600

    Add tests for cell and neighbour

---
+++ tests/cell_test.rb
@@ -0,0 +1,64 @@
+require 'minitest/autorun'
+require '../grid'
+require '../cell'
+
+class TestCell < Minitest::Test
+  def test_is_alive_method
+    assert alive_cell.alive?, 'Cell should be alive'
+  end
+
+  def test_is_dead_method
+    assert dead_cell.dead?, 'Cell should be dead'
+  end
+
+  def test_kill_method
+    assert_equal :dead, alive_cell.dead!
+    assert alive_cell.dead?, 'Cell should be dead'
+  end
+
+  def test_revive_method
+    assert_equal :alive, dead_cell.alive!
+    assert dead_cell.alive?, 'Cell should be alive'
+  end
+
+  def test_state_as_number_method
+    assert_equal 0, dead_cell.state_as_number
+    assert_equal 1, alive_cell.state_as_number
+  end
+
+  def test_assign_position_with_valid_arguments_method
+    position = { row: 2, col: 3 }
+    assert_equal(position, alive_cell.send(:assign_position, position))
+  end
+
+  def test_assign_position_with_invalid_arguments_method
+    assert_raises(ArgumentError) do
+      alive_cell.send :assign_position
+      assert_nil alive_cell.send :assign_position, { row: -2, col: -4 }
+    end
+  end
+
+  def test_assign_grid_with_valid_grid
+    alive_cell.grid = grid
+    assert_equal grid, alive_cell.grid
+  end
+
+  def test_assign_grid_with_invalid_grid
+    alive_cell.grid = :foo
+    assert_nil alive_cell.grid
+  end
+
+  private
+
+  def alive_cell
+    @alive_cell ||= Cell.new state: :alive, position: { row: 2, col: 1 }
+  end
+
+  def dead_cell
+    @dead_cell ||= Cell.new position: { row: 2, col: 2 }, grid: grid
+  end
+
+  def grid
+    @grid ||= Grid.new(8, 9)
+  end
+end
---
+++ tests/neighbour_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require_relative './support/hardcode_grid_support'
+require '../neighbour'
+require '../cell'
+
+class TestNeighbour < Minitest::Test
+  include HardcodeGridSupport
+
+  def test_around_method
+    neighbours_around = { up_left: [0, 0], up: [0, 1], up_right: [0, 2], left: [1, 0], right: [1, 2],
+                          down_left: [2, 0], down: [2, 1], down_right: [2, 2] }
+
+    assert_equal neighbours_around, neighbours.send(:around)
+  end
+
+  def test_check_method
+    assert_equal 2, neighbours.check!
+  end
+
+  private
+
+  def grid
+    @grid ||= Grid.new(3, 3, hardcode_grid).tap(&:assign_self_to_cells!)
+  end
+
+  def neighbours
+    @neighbours ||= Neighbour.new(grid.object[1][1])
+  end
+end
