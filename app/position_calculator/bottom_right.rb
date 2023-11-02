3ef10fadd62bac8e4811f1af452d7df8d4993634
Manuel Topete <mtopete.dev@gmail.com>
2023-11-01 22:55:52 -0600

    Creating PositionCalculator::BottomRight service class
    What?
    Now that we have the base, we can define the classes for each
    position. In this case: PositionCalculator::BottomRight.
    
    How?
    We have to create the PositionCalculator::BottomRight and make it inherit
    PositionCalculator::Base. Also, we're creating its specs, by doing
    this we're covering the PositionCalculator::Base code too.

---
+++ app/position_calculator/bottom_right.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module PositionCalculator
+  class BottomRight < Base
+    private
+
+    def calculate_position
+      @coord_x = coord_x + 1
+      @coord_y = coord_y + 1
+    end
+  end
+end
---
+++ spec/app/position_calculator/bottom_right_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'byebug'
+require 'rspec'
+
+require_relative '../../../app/position_calculator/bottom_right'
+
+describe PositionCalculator::BottomRight do
+  describe '#call' do
+    subject { described_class.new(coord_x:, coord_y:, max_x:, max_y:).call }
+
+    context 'when grid is 3 x 3' do
+      # 2 is max because we start counting from 0
+      let(:max_x) { 2 }
+      let(:max_y) { 2 }
+
+      context 'when cell x=1, y=1' do
+        let(:coord_x) { 1 }
+        let(:coord_y) { 1 }
+
+        it { is_expected.to eq([2, 2]) }
+      end
+
+      context 'when cell x=0, y=0' do
+        let(:coord_x) { 0 }
+        let(:coord_y) { 0 }
+
+        it { is_expected.to eq([1, 1]) }
+      end
+
+      context 'when cell x=0, y=2' do
+        let(:coord_x) { 0 }
+        let(:coord_y) { 2 }
+
+        it { is_expected.to eq([nil, nil]) }
+      end
+    end
+  end
+end
