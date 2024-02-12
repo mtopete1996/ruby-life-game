3c05ae02df95dea96b180cc123e9e5c28aeffc46
Manuel Topete <mtopete.dev@gmail.com>
2024-02-12 16:11:52 -0600

    Grid init in ./app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,5 @@ require 'zeitwerk'
 loader = Zeitwerk::Loader.new
 loader.push_dir('app')
 loader.setup
+
+Grid.start
--- app/grid.rb
+++ app/grid.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class Grid
-  # ROWS and COLS start at 0
+  # ROWS and COLS start at 0 and end at ROWS & COLS
   ROWS = 5
   COLS = 5
+
+  class << self
+    def init
+      new.object
+    end
+  end
 
   def object
     @object ||= Populator.call
